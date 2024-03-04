08ae1af6ed5431732be549c61107a4edf595099a
Joy Biswas <138972138+JoyBiswasgithub@users.noreply.github.com>
2024-03-04 13:55:10 +0530

    Add files via upload

---
+++ lang_det.py
@@ -0,0 +1,20 @@
+import pickle
+import streamlit as st
+
+
+import pickle
+
+pickle_file_path = "svc98.pkl"
+
+with open(pickle_file_path, "rb") as pickle_file:
+        Lrdetect_Model = pickle.load(pickle_file)
+
+
+st.title("Language Detection Tool")
+input_test = st.text_input("Enter Your Text Here")
+
+button_clicked = st.button("Get Language Name")
+res = ''
+if button_clicked:
+	res = Lrdetect_Model.predict([input_test])[0]
+st.write("# ", res)
