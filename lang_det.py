e0ff439cfaf4499e55c988f871152f95df4247c8
Joy Biswas <138972138+JoyBiswasgithub@users.noreply.github.com>
2024-03-04 16:16:46 +0530

    Update lang_det.py

--- lang_det.py
+++ lang_det.py
@@ -7,7 +7,7 @@ with open(pickle_file_path, "rb") as pickle_file:
         Lrdetect_Model = pickle.load(pickle_file)
 
 
-st.title("Language Detection Tool")
+st.title("LANGUAGE DETECTION TOOL")
 input_test = st.text_input("Enter Your Text Here")
 
 button_clicked = st.button("Get Language Name")
